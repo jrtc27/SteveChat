jrtc27/SteveChat

commit b63364dc4fba816c6b10660ebacfdfdf27535eb1

Allow partial player name matches

src/main/java/com/jrtc27/stevechat/Channel.java
@@ -120,7 +120,7 @@ public class Channel implements IPermissionDependency {
 		this.plugin.getServer().getConsoleSender().sendMessage(message);
 
 		for (final String name : this.members) {
-			final Player player = this.plugin.getServer().getPlayerExact(name);
+			final Player player = Util.getPlayer(name, false);
 			final Chatter recipient = this.plugin.channelHandler.chatterForPlayer(name);
 			if (player != null && (sender == null || !recipient.ignoring.contains(sender)) && this.inWorld(player)) {
 				player.sendMessage(message);

src/main/java/com/jrtc27/stevechat/command/BanCommand.java
@@ -60,7 +60,7 @@ public class BanCommand extends ChatCommandBase {
 			return true;
 		}
 
-		final Player onlinePlayer = this.plugin.getServer().getPlayerExact(player);
+		final Player onlinePlayer = Util.getPlayer(player, false);
 
 		if (channel.isBanned(player)) {
 			channel.unbanPlayer(player);

src/main/java/com/jrtc27/stevechat/command/KickCommand.java
@@ -60,7 +60,7 @@ public class KickCommand extends ChatCommandBase {
 			return true;
 		}
 
-		final Player onlinePlayer = this.plugin.getServer().getPlayerExact(player);
+		final Player onlinePlayer = Util.getPlayer(player, false);
 		final Chatter onlineChatter = this.plugin.channelHandler.chatterForPlayer(player);
 
 		channel.removeMember(player);

src/main/java/com/jrtc27/stevechat/command/MuteCommand.java
@@ -47,7 +47,7 @@ public class MuteCommand extends ChatCommandBase {
 				return true;
 			}
 
-			final Player onlinePlayer = this.plugin.getServer().getPlayerExact(player);
+			final Player onlinePlayer = Util.getPlayer(player, false);
 
 			if (channel.isMuted(player)) {
 				channel.unmutePlayer(player);
@@ -70,7 +70,7 @@ public class MuteCommand extends ChatCommandBase {
 			}
 
 			final Chatter chatter = this.plugin.channelHandler.chatterForPlayer(player);
-			final Player onlinePlayer = this.plugin.getServer().getPlayerExact(player);
+			final Player onlinePlayer = Util.getPlayer(player, false);
 
 			final boolean nowMuted;
 

src/main/java/com/jrtc27/stevechat/command/TellCommand.java
@@ -41,7 +41,7 @@ public class TellCommand extends ChatCommandBase {
 		if (recipientName.equalsIgnoreCase(Util.CONSOLE_COMMAND_SENDER_NAME)) {
 			recipient = this.plugin.getServer().getConsoleSender();
 		} else {
-			recipient = this.plugin.getServer().getPlayerExact(recipientName);
+			recipient = Util.getPlayer(recipientName, false);
 		}
 		if (recipient == null) {
 			sender.sendMessage(MessageColor.ERROR + "Unknown recipient: " + MessageColor.PLAYER + recipientName);

src/main/java/com/jrtc27/stevechat/command/WhoCommand.java
@@ -74,7 +74,7 @@ public class WhoCommand extends ChatCommandBase {
 
 		final List<String> visibleMembers = new ArrayList<String>();
 		for (final String member : channel.members) {
-			final Player player = this.plugin.getServer().getPlayerExact(member);
+			final Player player = Util.getPlayer(member, false);
 			if (player == null) {
 				continue;
 			}
@@ -110,7 +110,7 @@ public class WhoCommand extends ChatCommandBase {
 				}
 
 				final Chatter chatter = this.plugin.channelHandler.chatterForPlayer(member);
-				final Player player = this.plugin.getServer().getPlayerExact(member);
+				final Player player = Util.getPlayer(member, false);
 
 				if (player != null) { // No idea why it could be null now, as we checked in the previous loop, but may as well check
 					if (!channel.inWorld(player.getWorld().getName())) {

src/main/java/com/jrtc27/stevechat/Util.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
@@ -108,6 +109,10 @@ public class Util {
 
 	public static boolean isPermissionSet(final Player player, final String node) {
 		return player.isPermissionSet(node);
+	}
+
+	public static Player getPlayer(final String name, final boolean exact) {
+		return exact ? Bukkit.getServer().getPlayerExact(name) : Bukkit.getServer().getPlayer(name);
 	}
 
 	public static String getPrefix(final Player player) {

src/main/java/com/jrtc27/stevechat/command/SetCommand.java
@@ -19,6 +19,7 @@ import com.jrtc27.stevechat.Util;
 
 public class SetCommand extends ChatCommandBase {
 	private static List<String> knownCommands = new ArrayList<String>(Arrays.asList("name", "nick", "format", "password", "color", "radius", "qm-shortcut", "announce-activity"));
+
 	static {
 		Collections.sort(knownCommands, String.CASE_INSENSITIVE_ORDER);
 	}

commit a4c82d11a2dde9a61d07f39d8a8d31afaff2ef65

Fixed issues introduced by b63364dc4fba816c6b10660ebacfdfdf27535eb1

src/main/java/com/jrtc27/stevechat/Channel.java
@@ -120,7 +120,7 @@ public class Channel implements IPermissionDependency {
 		this.plugin.getServer().getConsoleSender().sendMessage(message);
 
 		for (final String name : this.members) {
-			final Player player = Util.getPlayer(name, false);
+			final Player player = Util.getPlayer(name, true);
 			final Chatter recipient = this.plugin.channelHandler.chatterForPlayer(name);
 			if (player != null && (sender == null || !recipient.ignoring.contains(sender)) && this.inWorld(player)) {
 				player.sendMessage(message);

src/main/java/com/jrtc27/stevechat/command/BanCommand.java
@@ -1,6 +1,8 @@
 package com.jrtc27.stevechat.command;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
@@ -60,7 +62,7 @@ public class BanCommand extends ChatCommandBase {
 			return true;
 		}
 
-		final Player onlinePlayer = Util.getPlayer(player, false);
+		final Player onlinePlayer = Util.getPlayer(player, true);
 
 		if (channel.isBanned(player)) {
 			channel.unbanPlayer(player);
@@ -76,6 +78,14 @@ public class BanCommand extends ChatCommandBase {
 			sender.sendMessage(MessageColor.PLAYER + player + MessageColor.INFO + " has been banned from " + channel.getColor() + channel.getName() + MessageColor.INFO + ".");
 			final Chatter onlineChatter = this.plugin.channelHandler.chatterForPlayer(player);
 			synchronized (onlineChatter) {
+				final Collection<String> toRemove = new HashSet<String>();
+				for (final String channelName : onlineChatter.channelsToJoin) {
+					final Channel toJoin = this.plugin.channelHandler.getChannelByName(channelName);
+					if (toJoin != null && toJoin.equals(channel)) {
+						toRemove.add(channelName);
+					}
+				}
+				onlineChatter.channelsToJoin.removeAll(toRemove);
 				if (channel.equals(onlineChatter.getActiveChannel())) {
 					onlineChatter.setActiveChannel(null);
 				}

src/main/java/com/jrtc27/stevechat/command/KickCommand.java
@@ -1,6 +1,8 @@
 package com.jrtc27.stevechat.command;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
@@ -60,12 +62,20 @@ public class KickCommand extends ChatCommandBase {
 			return true;
 		}
 
-		final Player onlinePlayer = Util.getPlayer(player, false);
+		final Player onlinePlayer = Util.getPlayer(player, true);
 		final Chatter onlineChatter = this.plugin.channelHandler.chatterForPlayer(player);
 
 		channel.removeMember(player);
 
 		synchronized (onlineChatter) {
+			final Collection<String> toRemove = new HashSet<String>();
+			for (final String channelName : onlineChatter.channelsToJoin) {
+				final Channel toJoin = this.plugin.channelHandler.getChannelByName(channelName);
+				if (toJoin != null && toJoin.equals(channel)) {
+					toRemove.add(channelName);
+				}
+			}
+			onlineChatter.channelsToJoin.removeAll(toRemove);
 			if (channel.equals(onlineChatter.getActiveChannel())) {
 				onlineChatter.setActiveChannel(null);
 			}

src/main/java/com/jrtc27/stevechat/command/MuteCommand.java
@@ -47,7 +47,7 @@ public class MuteCommand extends ChatCommandBase {
 				return true;
 			}
 
-			final Player onlinePlayer = Util.getPlayer(player, false);
+			final Player onlinePlayer = Util.getPlayer(player, true);
 
 			if (channel.isMuted(player)) {
 				channel.unmutePlayer(player);
@@ -70,7 +70,7 @@ public class MuteCommand extends ChatCommandBase {
 			}
 
 			final Chatter chatter = this.plugin.channelHandler.chatterForPlayer(player);
-			final Player onlinePlayer = Util.getPlayer(player, false);
+			final Player onlinePlayer = Util.getPlayer(player, true);
 
 			final boolean nowMuted;
 

src/main/java/com/jrtc27/stevechat/command/TellCommand.java
@@ -73,7 +73,7 @@ public class TellCommand extends ChatCommandBase {
 		recipient.sendMessage(this.plugin.channelHandler.formatPM(true, sender, sender, message));
 
 		if (recipient instanceof Player) {
-			final Chatter chatter = this.plugin.channelHandler.chatterForPlayer(recipientName);
+			final Chatter chatter = this.plugin.channelHandler.chatterForPlayer(recipient.getName());
 
 			if (sender instanceof Player) {
 				chatter.setLastConverser(sender.getName());

src/main/java/com/jrtc27/stevechat/command/WhoCommand.java
@@ -74,7 +74,7 @@ public class WhoCommand extends ChatCommandBase {
 
 		final List<String> visibleMembers = new ArrayList<String>();
 		for (final String member : channel.members) {
-			final Player player = Util.getPlayer(member, false);
+			final Player player = Util.getPlayer(member, true);
 			if (player == null) {
 				continue;
 			}
@@ -110,7 +110,7 @@ public class WhoCommand extends ChatCommandBase {
 				}
 
 				final Chatter chatter = this.plugin.channelHandler.chatterForPlayer(member);
-				final Player player = Util.getPlayer(member, false);
+				final Player player = Util.getPlayer(member, true);
 
 				if (player != null) { // No idea why it could be null now, as we checked in the previous loop, but may as well check
 					if (!channel.inWorld(player.getWorld().getName())) {